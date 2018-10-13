d1463989855bd64e4985f4484e78f109ab57c57d
William Duong <Will@Williams-MacBook-Pro-2.local>
2018-10-12 21:39:07 -0400

    Modified Testing and Anime files.

--- Anime.java
+++ Anime.java
@@ -1,5 +1,6 @@
 public class Anime {
 	public static void main(String[] args) {
 	System.out.println("KIRITOOOO");
-	}
+	System.out.println("ASUNAAA");	
+}
 }
--- Testing.java
+++ Testing.java
@@ -1,5 +1,6 @@
 public class Testing {
 	public static void main(String[] args) {
 	System.out.println("Hello World");
-	}
+	System.out.println("Omae Wa Mou Shinderu");	
+	}	
 }
